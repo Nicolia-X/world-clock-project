ee152a34765c700d56777500d7f11c22da1313d4
Nicolia-X <168901828+Nicolia-X@users.noreply.github.com>
2024-07-09 19:24:41 +0200

    Installed moment libraries on html for real-life data and coded JS for interaction
    Installed moment.js and moment.timezone.js on my html then built/coded the interaction on JavaScript for real-life data

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function updateData() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+
+  //Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateData();
+setInterval(updateData, 1000);
