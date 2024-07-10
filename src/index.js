aa0861c1fefebe474699703fdbf3fbfbf46ecfbd
Nicolia-X <168901828+Nicolia-X@users.noreply.github.com>
2024-07-10 23:48:10 +0200

    Updated html file with cities as well as link to Netlify and updated the real-time data of cities on the JavaScript filefile

--- src/index.js
+++ src/index.js
@@ -12,6 +12,19 @@ function updateData() {
     );
   }
 
+  //Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  if (johannesburgElement) {
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
   //Sydney
   let sydneyElement = document.querySelector("#sydney");
   if (sydneyElement) {
